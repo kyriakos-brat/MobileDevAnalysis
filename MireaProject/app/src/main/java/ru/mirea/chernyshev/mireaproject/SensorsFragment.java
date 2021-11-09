3415f3e61eedfbf0026636ecf1e6e3c734593eee
kyriakos-brat <kiryarik99@gmail.com>
2021-11-09 23:18:39 +0300

    SensorsFragment added

---
+++ MireaProject/app/src/main/java/ru/mirea/chernyshev/mireaproject/SensorsFragment.java
@@ -0,0 +1,115 @@
+package ru.mirea.chernyshev.mireaproject;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SensorsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SensorsFragment extends Fragment implements SensorEventListener {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private TextView azimuthTextView;
+    private TextView pitchTextView;
+    private TextView rollTextView;
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SensorsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SensorsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SensorsFragment newInstance(String param1, String param2) {
+        SensorsFragment fragment = new SensorsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometerSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sensors, container, false);
+
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        azimuthTextView = (TextView) view.findViewById(R.id.textViewAzimuth);
+        pitchTextView = (TextView) view.findViewById(R.id.textViewPitch);
+        rollTextView = (TextView) view.findViewById(R.id.textViewRoll);
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (Sensor.TYPE_ACCELEROMETER == event.sensor.getType()) {
+            float valueAzumuth = event.values[0];
+            float valuePitch = event.values[1];
+            float valueRoll = event.values[2];
+
+            azimuthTextView.setText("Azimuth: " + valueAzumuth);
+            pitchTextView.setText("Pitch: " + valuePitch);
+            rollTextView.setText("Roll: " + valueRoll);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
